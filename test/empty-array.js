b47a08eb31bb79be4c6324f7883efe86b4aec4ef
Feross Aboukhadijeh <feross@feross.org>
2014-04-12 18:42:20 -0700

    add failing test for empty array

---
+++ test/empty-array.js
@@ -0,0 +1,16 @@
+var parallel = require('../')
+var test = require('tape')
+
+test('empty tasks array', function (t) {
+  t.plan(1)
+
+  parallel([], function (err) {
+    t.error(err)
+  })
+})
+
+test('empty tasks array and no callback', function (t) {
+  parallel([])
+  t.pass('did not throw')
+  t.end()
+})
